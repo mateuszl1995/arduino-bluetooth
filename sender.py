84c960a742b06363e9d82771b0593caed2e555ad
root <bartekgier1@gmail.com>
2018-01-13 18:27:41 +0100

    Not ok

--- sender.py
+++ sender.py
@@ -2,13 +2,16 @@ import serial
 import time
 
 print("Start")
-port="/dev/tty.HC-06-DevB" #This will be different for various devices and on windows it will probably be a COM port.
+port="/dev/rfcomm1" #This will be different for various devices and on windows it will probably be a COM port.
 bluetooth=serial.Serial(port, 9600)#Start communications with the bluetooth unit
+
 print("Connected")
 bluetooth.flushInput() #This gives the bluetooth a little kick
-for i in range(5): #send 5 groups of data to the bluetooth
-	print("Ping")
-	bluetooth.write(b"BOOP "+str.encode(str(i)))#These need to be bytes not unicode, plus a number
+print("Ping")
+while 1==1:
+	x = raw_input('Provide string: ');
+	print x
+	bluetooth.write(x)#These need to be bytes not unicode, plus a number
 	input_data=bluetooth.readline()#This reads the incoming data. In this particular example it will be the "Hello from Blue" line
 	print(input_data.decode())#These are bytes coming in so a decode is needed
 	time.sleep(0.1) #A pause between bursts
