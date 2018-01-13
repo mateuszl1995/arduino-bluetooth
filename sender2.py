e1f056005aa4105395315cfc8bf81b4f6d423a97
Bartosz Gierczak <bartekgier1@gmail.com>
2018-01-13 18:10:21 +0100

    OK

--- sender2.py
+++ sender2.py
@@ -1,20 +1,31 @@
 from tkinter import *
 import bluetooth
+from os import walk
+from os.path import isfile, join
 
 port = 1
-nearby_devices = bluetooth.discover_devices()
-for i in nearby_devices:
-	if bluetooth.lookup_name( i ) == 'HC-05':
-		bd_addr = i
 
-sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-sock.connect((bd_addr, port))
+def findDevice(name):
+	nearby_devices = bluetooth.discover_devices()
+	for i in nearby_devices:
+		if bluetooth.lookup_name( i ) == name:
+			return i;
 
-print('Connected');
+def main(port):
+	port = 1
+	sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+	sock.connect((findDevice('HC-05'), port))
+	print('Connected')
+	communication(sock)
+	sock.close()
 
-while 1==1:
-	data = sock.recv(1024);
-	print(str(data.decode("UTF-8")));
-	sock.send(data)
 
-sock.close()
+def communication(sock):
+	while 1==1:
+		data = sock.recv(1024);
+		print(str(data.decode("UTF-8")));
+		sock.send(data)
+def getFilesInDir(path):
+	return [f for f in listdir(path) if isfile(join(path, f))]
+
+print(getFilesInDir('melodies'))
